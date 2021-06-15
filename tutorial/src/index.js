7825971c931cff109db9d4b8557de9f76dda20b2
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2021-06-15 22:14:31 +0300

    props learn

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,25 +4,32 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
+const firstBook = {
+  title: "Why a Daughter Needs a Dad",
+  authore: "Gregory Lang",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81Y8iml53ML._AC_UL200_SR200,200_.jpg"
+}
+const secondBook = {
+  title: "The Last Thing He Told Me: A Novel",
+  authore: "Laura Dave",
+  img: "https://images-na.ssl-images-amazon.com/images/I/51b-JoV-1xS._SX329_BO1,204,203,200_.jpg"
+}
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book img={firstBook.img} authore={firstBook.authore} title={firstBook.title}/>
+      <Book img={secondBook.img} authore={secondBook.authore} title={secondBook.title}/>
     </section>
   );
 }
-const authore = 'Gregory Lang';
 
-const Book = () => {
-  const title = 'Why a Daughter Needs a Dad';
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81Y8iml53ML._AC_UL200_SR200,200_.jpg"
-        alt=""
-      />
-      <h2>{title}</h2>
-      <h3>{authore}</h3>
+      <img src={props.img} alt="" />
+      <h2>{props.title}</h2>
+      <h3>{props.authore}</h3>
     </article>
   );
 };
